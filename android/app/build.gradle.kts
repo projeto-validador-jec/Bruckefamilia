3e6912c20e407fdb83aa828c2d539551859b0f30
Seu Nome <seuemail@exemplo.com>
2026-07-30 04:53:18 -0300

    feat: implementa cripto ponta a ponta e chamadas ZegoCloud limpo

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,56 @@
+import java.util.Properties
+import java.io.FileInputStream
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { stream ->
+        localProperties.load(stream)
+    }
+}
+
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+android {
+    namespace = "com.arxforense.bruckefamilia"
+    compileSdk = 36
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.arxforense.bruckefamilia"
+        minSdk = 24
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
---
+++ lib/call_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:zego_cloud_uikit_prebuilt_call/zego_cloud_uikit_prebuilt_call.dart';
+
+class CallPage extends StatelessWidget {
+  final String callID;
+  final String userID;
+  final String userName;
+
+  const CallPage({
+    Key? key,
+    required this.callID,
+    required this.userID,
+    required this.userName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ZegoUIKitPrebuiltCall(
+      appID: 1607164961,
+      appSign: "52762dec2c3841f4201e46ef0fd89ed778d765830914e8a08e1d0175489f60a7",
+      userID: userID,
+      userName: userName,
+      callID: callID,
+      config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall(),
+    );
+  }
+}
